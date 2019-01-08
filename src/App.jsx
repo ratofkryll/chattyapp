bd48a446d88e63f1d63a14643f94d1eca0f1ecfc
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-08 20:08:33 +0000

    added state to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,23 @@ import ChatBar from './ChatBar.jsx';
 
 {/* Render navbar */}
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+    
   render() {
     return (
       <nav className="navbar">
