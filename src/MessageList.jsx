f2be26b1fd5c6a3bcc27e40f26a436ce3dc2bdbd
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-08 20:35:57 +0000

    passed App messages state to Messages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,9 +3,12 @@ import { Message, Notification } from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const message = this.props.messages.map(message => {
+      return (<Message username={ message.username } content={ message.content } />);
+    });
     return (
       <main className="messages">
-        <Message />
+        { message }
         <Notification />
       </main>
     );
