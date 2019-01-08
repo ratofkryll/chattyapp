c45b11b403c191efacf1110c6deef49cfd6e526f
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-08 20:18:05 +0000

    passed currentUser name to ChatBox & set to display if name present

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import ChatBar from './ChatBar.jsx';
 
 {/* Render navbar */}
 class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    );
+  }
+}
+
+{/* Render all app components */}
+class App extends Component {
   constructor(props) {
     super(props);
 
@@ -20,24 +31,14 @@ class Navbar extends Component {
         }
       ]
     }
-    
-  render() {
-    return (
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
-    );
   }
-}
 
-{/* Render all app components */}
-class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={ this.state.messages } />
+        <ChatBar currentUser={ this.state.currentUser.name } />
       </div>
     );
   }
