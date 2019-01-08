2b9b3b571cd742f72b35c8b0d8161e81635eb3f9
Gioanna Rheumer <ratofkryll@gmail.com>
2019-01-08 19:18:33 +0000

    created jsx modules from starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
+{/* Render navbar */}
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    );
+  }
+}
+
+{/* Render all app components */}
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
